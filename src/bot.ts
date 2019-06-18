0f9eefe07deead4c7063f91df56590e33a65465a
maymike321 <yamekim@comcast.net>
2019-06-17 21:10:58 -0600

    add get id command handler

--- src/bot.ts
+++ src/bot.ts
@@ -3,10 +3,12 @@ import { remindCommandHandler } from './remindCommandHandler.js';
 import { pretendToTypeCommandHandler } from './pretendToTypeCommandHandler.js';
 import { customCommandHandler } from './customCommandHandler.js';
 import { deleteCommandHandler } from './deleteCommandHandler.js';
+import { getIdCommandHandler } from './getIdCommandHandler.js';
 
 const commandBot = new CommandBot(process.argv[2]);
 commandBot.addCommandHandler(remindCommandHandler);
 commandBot.addCommandHandler(pretendToTypeCommandHandler);
 commandBot.addCommandHandler(customCommandHandler);
 commandBot.addCommandHandler(deleteCommandHandler);
+commandBot.addCommandHandler(getIdCommandHandler);
 commandBot.run();
---
+++ src/getIdCommandHandler.ts
@@ -0,0 +1,30 @@
+import { CommandAction, CommandHandler } from "./commandBot";
+
+const getIdCommandAction: CommandAction = (context, words) => {
+    const { userId, channelId, bot } = context;
+    if (words.length === 0) {
+        bot.sendMessage({
+            to: channelId,
+            message: `Your user id is ${userId}`
+        });
+        return;
+    }
+    const possibleUser = words[0];
+    if (words.length !== 1 || (!possibleUser.startsWith("@<") || !possibleUser.endsWith(">"))) {
+        bot.sendMessage({
+            to: channelId,
+            message: `Usage:  !getId @user`
+        });
+        return;
+    }
+    bot.sendMessage({
+        to: channelId,
+        message: `${possibleUser}'s user id is: ${possibleUser.substring(2, possibleUser.length - 1)}`
+    });
+}
+
+export const getIdCommandHandler: CommandHandler = {
+    commandName: 'getId',
+    commandAction: getIdCommandAction,
+    description: 'Gets the id of a given user.'
+}
